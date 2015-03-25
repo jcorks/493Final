e17f72a73621e8ba5a4bbb275b6cdbeda7859525
Tyler <tylergp@umich.edu>
2015-03-25 18:45:57 -0400

    Tyler's jenga block

---
+++ Jenga/Assets/JengaBlockScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JengaBlockScript : MonoBehaviour {
+	public GameObject	jengaBlockPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Instantiate (jengaBlockPrefab);
+	}
+}

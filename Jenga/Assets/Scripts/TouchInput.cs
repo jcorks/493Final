701e389a1168affc48ed74cccf9ddeceabf11c5b
jcorks <jcorkery@umich.edu>
2015-04-04 19:54:22 -0400

    Improved swipe detection

--- Jenga/Assets/Scripts/TouchInput.cs
+++ Jenga/Assets/Scripts/TouchInput.cs
@@ -3,22 +3,33 @@ using System.Collections;
 
 public class TouchInput : MonoBehaviour {
 	static float pointerX, pointerY;
+	static Vector2 lastpos;
+	static Vector2 thispos;
+
+	float threshold = 100f;
+	float dragMinimumTimeSeconds = .07f;	
+
+	static bool swipeLeftState = false;
+	static bool swipeRightState = false;
+	static bool swipeDownState = false;
+	static bool swipeUpState = false;
+	float time = 0f;
 	
 	public static bool swipeLeft() {
-		return Input.GetKeyDown (KeyCode.A);
+		return Input.GetKeyDown (KeyCode.A) || swipeLeftState;
 	}
 	
 	
 	public static bool swipeRight() {
-		return Input.GetKeyDown (KeyCode.D);
+		return Input.GetKeyDown (KeyCode.D) || swipeRightState;
 	}
 	
 	public static bool swipeUp() {
-		return Input.GetKeyDown (KeyCode.W);
+		return Input.GetKeyDown (KeyCode.W) || swipeUpState;
 	}
 	
 	public static bool swipeDown() {
-		return Input.GetKeyDown (KeyCode.S);
+		return Input.GetKeyDown (KeyCode.S) || swipeDownState;
 	}
 	
 	public static bool pinchZoomIn() {
@@ -34,7 +45,8 @@ public class TouchInput : MonoBehaviour {
 			pointerX = Input.mousePosition.x;
 			pointerY = Input.mousePosition.y;
 			return true;
-		} else if (Input.GetTouch (0).tapCount == 1) {
+		} else if (Input.touchSupported &&    
+		           Input.GetTouch (0).tapCount == 1) {
 			pointerX = Input.GetTouch (0).position.x;
 			pointerY = Input.GetTouch (0).position.y;
 			return true;
@@ -51,4 +63,44 @@ public class TouchInput : MonoBehaviour {
 		return pointerY;
 	}
 
+
+	void Update() {
+
+
+
+
+		swipeLeftState = false;
+		swipeRightState = false;
+		swipeUpState = false;
+		swipeDownState = false;
+		time += Time.deltaTime;
+		if (Input.touchSupported && Input.GetTouch (0).tapCount > 0) {
+			Touch curTouch = Input.GetTouch (0);
+
+
+			if (curTouch.phase == TouchPhase.Began) {
+				lastpos = curTouch.position;
+				time = 0f;
+			} else if (curTouch.phase == TouchPhase.Moved) {
+				thispos = curTouch.position;
+
+			} else if (curTouch.phase == TouchPhase.Ended) {
+				if (time < dragMinimumTimeSeconds) return;
+
+				Vector2 delta = thispos - lastpos;
+
+				if (delta.x < -threshold) {
+					swipeLeftState = true; 
+				} else if (delta.x > threshold) {
+					swipeRightState = true;
+				}
+
+				if (delta.y < -threshold) {
+					swipeUpState = true;
+				} else if (delta.y > threshold) {
+					swipeDownState = true;
+				}
+			}
+		} 
+	}
 }

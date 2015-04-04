4e16e8010a0cdea10707370da5319c2054234328
jcorks <jcorkery@umich.edu>
2015-04-04 03:11:15 -0400

    Fixed view input to stop going crazy. Added camera prefab

--- Jenga/Assets/MainTurn/TurnManager.cs
+++ Jenga/Assets/MainTurn/TurnManager.cs
@@ -3,7 +3,18 @@ using System.Collections;
 
 public class TurnManager : MonoBehaviour {
 
+	public enum TurnPhase
+	{
+		ChoosePiece,
+		DragPiece,
+		ReplacePiece
+
+	};
+
+
 	public float radius = .17f;
+	public float degreeDelta = 45f;
+	public float radiusDeltaRatio = .5f;
 	Vector3 towerCenter = Vector3.zero;
 
 	float pitch =45f,
@@ -11,47 +22,45 @@ public class TurnManager : MonoBehaviour {
 		  roll  =45f;
 
 
-	float degreeDelta = 45f;
+
 
 	Vector3 targetPos;
 	Vector3 targetRot;
 
+	TurnPhase phase = TurnPhase.ChoosePiece;
+
 	// Use this for initialization
 	void Start () {
 		GameObject tower = GameObject.FindGameObjectWithTag ("Tower");
-		towerCenter = tower.transform.position;
+		if (tower == null) {
+			Debug.Log ("WARNING! TurnManager could not find the tower prefab to center itself!");
+		} else {
+			towerCenter = tower.transform.position;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (TouchInput.swipeLeft())
-			pitch += degreeDelta;
-
-		if (TouchInput.swipeRight ())
-			pitch -= degreeDelta;
-
-		if (TouchInput.swipeUp ())
-			roll += degreeDelta;
-
-		if (TouchInput.swipeDown ())
-			roll -= degreeDelta;
-
-		if (TouchInput.pinchZoomOut()) {
-			radius += radius*.05f;
-		}
-		if (TouchInput.pinchZoomIn()) {
-			radius -= radius*.05f;
-		}
+		if (phase == TurnPhase.ChoosePiece)
+			ChoosePieceUpdate ();
 
 
 	}
 
+
 	void FixedUpdate() {
 
-		if (roll > 360f) roll -=360f;
-		if (pitch > 360f) roll -=360f;
-		if (yaw > 360f) yaw -=360f;
+		// First bind values to 0 - 360 to prevent false lerping
+		if (roll >= 360f + transform.eulerAngles.x) roll -=360f;
+		if (pitch >= 360f + transform.eulerAngles.y) pitch -=360f;
+		if (yaw >= 360f + transform.eulerAngles.z) yaw -=360f;
 
+		if (roll <= -360f + transform.eulerAngles.x) roll +=360f;
+		if (pitch <= -360f + transform.eulerAngles.y) pitch +=360f;
+		if (yaw <= -360f + transform.eulerAngles.z) yaw +=360f;
+
+
+		// Then ease BOTH angle rotation and position movement
 		Vector3 targetRot = new Vector3 (roll, pitch, yaw);
 		Vector3 curRot = transform.rotation.eulerAngles;
 		Vector3 easedRot = Vector3.Lerp (curRot, targetRot,  .1f);
@@ -61,4 +70,31 @@ public class TurnManager : MonoBehaviour {
 		Vector3 target = towerCenter + Quaternion.Euler (roll, pitch, yaw) * new Vector3 (0, 0, -radius);
 		transform.position = Vector3.Lerp (transform.position, target, .1f);
 	}
+
+
+	
+
+	
+	
+	void ChoosePieceUpdate() {
+		if (TouchInput.swipeLeft())
+			pitch += degreeDelta;
+		
+		if (TouchInput.swipeRight ())
+			pitch -= degreeDelta;
+		
+		if (TouchInput.swipeUp ())
+			roll += degreeDelta;
+		
+		if (TouchInput.swipeDown ())
+			roll -= degreeDelta;
+		
+		if (TouchInput.pinchZoomOut()) {
+			radius += radius*radiusDeltaRatio;
+		}
+		if (TouchInput.pinchZoomIn()) {
+			radius -= radius*radiusDeltaRatio;
+		}
+	}
+
 }

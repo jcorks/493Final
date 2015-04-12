1401e7aae1ba59d197bff73fc40f3b422e6eaf4f
Tyler <tylergp@umich.edu>
2015-04-12 14:08:45 -0400

    Tyler's New Scene!

---
+++ Jenga/Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDrag : MonoBehaviour {
+
+	float distance = 0.1f;
+
+	void OnMouseDrag() {
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+		transform.position = objPosition;
+	}
+
+}

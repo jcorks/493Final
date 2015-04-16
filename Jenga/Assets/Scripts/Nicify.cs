0de08b54f228a8e6f2feec88e158f690edfe8b63
jcorks <jcorkery@umich.edu>
2015-04-16 02:54:26 -0400

    Text for player and player logic

---
+++ Jenga/Assets/Scripts/Nicify.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nicify : MonoBehaviour {
+	float time;
+	Color col;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time > 1) {
+			time = 0;
+			col = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f), 1);
+		}
+		GetComponent<TextMesh> ().color = Color.Lerp (GetComponent<TextMesh> ().color, col, .05f);
+	}
+}

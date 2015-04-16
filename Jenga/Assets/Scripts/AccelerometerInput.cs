8df192ab528e7fd3d5dd876bef9fe7b1699451f8
JoshSauter <sauterj@umich.edu>
2015-04-15 21:49:40 -0400

    New Table Accelerometer Happy Fun-Times

--- Jenga/Assets/Scripts/AccelerometerInput.cs
+++ Jenga/Assets/Scripts/AccelerometerInput.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class AccelerometerInput : MonoBehaviour {
-
+	public float forceValue = .0001f; //1/10,000
+	float movingThreshold = 0.1f;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +11,24 @@ public class AccelerometerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.gameObject.GetComponent<Rigidbody>().AddForce(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z, ForceMode.Acceleration);
+		Vector3 forceVector = Input.acceleration;
+		if (forceVector.x < movingThreshold)
+			forceVector.x = 0;
+		if (forceVector.y < movingThreshold)
+			forceVector.y = 0;
+		if (forceVector.z < movingThreshold)
+			forceVector.z = 0;
+		
+		
+		this.gameObject.GetComponent<Rigidbody>().AddForce(forceVector, ForceMode.Acceleration);
+		/*if (Input.GetKey(KeyCode.I)){
+			this.gameObject.GetComponent<Rigidbody>().AddForce(0,forceValue,0,ForceMode.Acceleration);
+		}
+		if (Input.GetKey(KeyCode.J)){
+			this.gameObject.GetComponent<Rigidbody>().AddForce(forceValue,0,0,ForceMode.Acceleration);
+		}
+		if (Input.GetKey(KeyCode.K)){
+			this.gameObject.GetComponent<Rigidbody>().AddForce(-forceValue,0,0,ForceMode.Acceleration);
+		}*/
 	}
 }

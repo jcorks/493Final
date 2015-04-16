a554b418fe6eba860f87dd96407b75b2c90618eb
JoshSauter <sauterj@umich.edu>
2015-04-16 01:30:45 -0400

    Sorry

---
+++ Jenga/Assets/Scripts/MusicController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+	public static bool musicEnabled = true;
+	public static bool soundEffectsEnabled = true;
+	
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	public void enableMusic(){
+		musicEnabled = true;
+	}
+	
+	public void disableMusic(){
+		musicEnabled = false;
+	}
+	
+	public void enableSoundEffects(){
+		soundEffectsEnabled = true;
+	}
+	
+	public void disableSoundEffects(){
+		soundEffectsEnabled = false;
+	}
+}

f7dd0d6b05548a29e45d16aa6c5bd351c9e77aa4
JoshSauter <sauterj@umich.edu>
2015-04-16 02:19:36 -0400

    Sound effect v1

---
+++ Jenga/Assets/Scripts/BlockHitSound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockHitSound : MonoBehaviour {
+	float threshold = 0.5f;
+	AudioSource soundEffect;
+	
+	// Use this for initialization
+	void Start () {
+		soundEffect = this.gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter(Collision other){
+		if (other.relativeVelocity.magnitude > threshold){
+			soundEffect.Play();
+			soundEffect.volume = other.relativeVelocity.magnitude/4f;
+			soundEffect.pitch = Random.Range(0.9f,1.1f);
+		}
+	}
+}

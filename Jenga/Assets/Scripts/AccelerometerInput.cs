79c41386f5d69233cc499a671894d6ceb5dd8dfb
JoshSauter <sauterj@umich.edu>
2015-04-15 20:53:14 -0400

    Accelerometer v1

---
+++ Jenga/Assets/Scripts/AccelerometerInput.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.GetComponent<Rigidbody>().AddForce(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z, ForceMode.Acceleration);
+	}
+}
